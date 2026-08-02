7a791dd6f9299d44ec49e8bf84f0392cc8244903
sreejith7340 <sreejithsaji36@gmail.com>
2026-08-02 12:45:05 +0530

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/anna12/",
   server: {
     port: 3000,
-    open: true
-  }
+    open: true,
+  },
 });
